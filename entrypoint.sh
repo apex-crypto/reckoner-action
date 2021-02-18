e1181037e9feb4952d56756a5e6101f56db3745b
Caroll George <cgeorge@peak6.com>
2021-02-18 13:51:47 -0600

    testing exit status

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,14 @@
 #!/bin/sh -l
 
-echo "$1"
-eval $1
+exit 1
+# echo "$1"
+# eval $1
 
-if [ $? -eq 0 ];then
-   exit 0
-else
-   exit 1
-fi
+# if [ $? -eq 0 ];then
+#    exit 0
+# else
+#    exit 1
+# fi
 
 time=$(date)
 echo "::set-output name=time::$time"
